755fa34dd26046fc39bbfaf4676a147f2d6750f2
Sandeepku8211 <monuku821110@gmail.com>
2025-03-22 07:07:55 -0700

    add button funcnality and more and add router each cards

--- src/components/pokedex/Pokedex.jsx
+++ src/components/pokedex/Pokedex.jsx
@@ -1,19 +1,25 @@
 import axios from 'axios'
+import './Pokedex.css'
 import React, { useEffect, useState } from 'react'
 import Pokemon from '../pokemon/Pokemon'
 
 const Pokedex = () => {
     const [isloading,setIsloading]=useState(true)
     const [pokedex,setPokedex]=useState([])
+    const [pokedexurl,SetPokedexurl] = useState('https://pokeapi.co/api/v2/pokemon')
+    const [nexturl,setNexturl]=useState('');
+    const [preurl,setPreurl]=useState('')
 
 async function downLoadPokeapi(){
-    const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
-    // console.log(response)
+    const response = await axios.get(pokedexurl);
+    setNexturl(response.data.next);
+    setPreurl(response.data.previous)
+    
     const pokemonResult= await response.data.results;
     const pokemonurl = pokemonResult.map
     ((pokemon)=> axios.get(pokemon.url));
     const pokemonData = await axios.all(pokemonurl)
-    console.log(pokemonData)
+ 
     const res = pokemonData.map((pokedata)=>{
         const pokemon = pokedata.data;
         return{
@@ -26,7 +32,7 @@ async function downLoadPokeapi(){
     })
 
     setPokedex(res)
-    console.log(pokemonData)
+    
 
     setIsloading(false)
 
@@ -34,13 +40,17 @@ async function downLoadPokeapi(){
 
 useEffect(()=>{
     downLoadPokeapi()
-},[])
+},[pokedexurl])
   return (
-    <div>
+    <div className='container'>
    <div className='wrap'>
    {
         (isloading)?"loadin ..":pokedex.map((p)=><Pokemon name={p.name} image={p.image}key={p.id} type={p.type} id={p.id}/>)
     }
+   </div>
+   <div className='btn'>
+    <button disabled={preurl==null} onClick={()=>SetPokedexurl(preurl)} className='p-4 m-4 bg-blue-500 text-white'>pre</button>
+    <button disabled={nexturl==null} onClick={()=>SetPokedexurl(nexturl)} className='p-4 m-4 bg-blue-500 text-white'>next</button>
    </div>
     </div>
   )
