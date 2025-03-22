a4dde697fa5f15b217c1ee28f634e5121fc5b354
Sandeepku8211 <monuku821110@gmail.com>
2025-03-22 07:27:51 -0700

    add details page of all pokemon

--- src/components/pokemondetails/PokemonDetails.jsx
+++ src/components/pokemondetails/PokemonDetails.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const PokemonDetails = () => {
+  const {name,id}= useParams();
+  const [pokemonde,setPokemonde]= useState({});
+
+  async function dowloadPokemonDe() {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    console.log(response)
+
+    setPokemonde({
+      name:response.data.name,
+      image:response.data.sprites.front_default,
+      height:response.data.height,
+      weight:response.data.weight,
+      types:response.data.types.map((t)=>t.type.name)
+
+    })
+    
+  }
+
+  useEffect(()=>{
+    dowloadPokemonDe()
+  })
   
   return (
-    <div>PokemonDetails</div>
+    <div>
+      <div> <img src={pokemonde.image} alt='data is dow' /></div>
+      <div>{pokemonde.name}</div>
+      <div>{pokemonde.height}</div>
+      <div>{pokemonde.weight}</div>
+      <div>{pokemonde.types}</div>
+    </div>
   )
 }
 
