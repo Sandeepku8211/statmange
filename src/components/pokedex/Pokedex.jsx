a6cf6f821296b432949fe28b000e3e999d46126a
Sandeepku8211 <monuku821110@gmail.com>
2025-03-23 05:42:03 -0700

    maneging state complete

--- src/components/pokedex/Pokedex.jsx
+++ src/components/pokedex/Pokedex.jsx
@@ -4,18 +4,29 @@ import React, { useEffect, useState } from 'react'
 import Pokemon from '../pokemon/Pokemon'
 
 const Pokedex = () => {
-    const [isloading,setIsloading]=useState(true)
-    const [pokedex,setPokedex]=useState([])
-    const [pokedexurl,SetPokedexurl] = useState('https://pokeapi.co/api/v2/pokemon')
-    const [nexturl,setNexturl]=useState('');
-    const [preurl,setPreurl]=useState('')
+    // const [isloading,setIsloading]=useState(true)
+    // const [pokedex,setPokedex]=useState([])
+    // const [pokedexurl,SetPokedexurl] = useState('https://pokeapi.co/api/v2/pokemon')
+    // const [nexturl,setNexturl]=useState('');
+    // const [preurl,setPreurl]=useState('')
+
+    const [pokeListstate,setPokeliststate]=useState({
+        pokedex:[],
+        isloading:true,
+        
+        pokedexurl:'https://pokeapi.co/api/v2/pokemon',
+        nexturl:'',
+        preurl:''
+    })
 
  
 
 async function downLoadPokeapi(){
-    const response = await axios.get(pokedexurl);
-    setNexturl(response.data.next);
-    setPreurl(response.data.previous)
+    setPokeliststate({...pokeListstate,isloading:true})
+    const response = await axios.get(pokeListstate.pokedexurl);
+    setPokeliststate({...pokeListstate, nexturl:response.data.next,preurl:response.data.previous})
+    // setPreurl(response.data.previous)
+   
     
     const pokemonResult= await response.data.results;
     const pokemonurl = pokemonResult.map
@@ -33,26 +44,26 @@ async function downLoadPokeapi(){
         }
     })
 
-    setPokedex(res)
+    // setPokedex(res)
+    setPokeliststate({...pokeListstate,pokedex:res,isloading:false})
     
-
-    setIsloading(false)
+    // setIsloading(false)
 
 }
 
 useEffect(()=>{
     downLoadPokeapi()
-},[pokedexurl])
+},[pokeListstate.pokedexurl])
   return (
     <div className='container'>
    <div className='wrap'>
    {
-        (isloading)?"loadin ..":pokedex.map((p)=><Pokemon name={p.name} image={p.image}key={p.id} type={p.type} id={p.id}/>)
+        (pokeListstate.isloading)?"loadin ..":pokeListstate.pokedex.map((p)=><Pokemon name={p.name} image={p.image}key={p.id} type={p.type} id={p.id}/>)
     }
    </div>
    <div className='btn'>
-    <button disabled={preurl==null} onClick={()=>SetPokedexurl(preurl)} className='p-4 m-4 bg-blue-500 text-white'>pre</button>
-    <button disabled={nexturl==null} onClick={()=>SetPokedexurl(nexturl)} className='p-4 m-4 bg-blue-500 text-white'>next</button>
+    <button disabled={pokeListstate.preurl==null} onClick={()=>setPokeliststate({...pokeListstate, pokedexurl:pokeListstate.preurl})} className='p-4 m-4 bg-blue-500 text-white'>pre</button>
+    <button disabled={pokeListstate.nexturl==null} onClick={()=>setPokeliststate({...pokeListstate, pokedexurl:pokeListstate.nexturl})} className='p-4 m-4 bg-blue-500 text-white'>next</button>
    </div>
     </div>
   )
